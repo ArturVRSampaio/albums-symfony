59695713bf2ed36d726598bcdb000a23b41d6107
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-20 15:53:29 -0300

    findall problem

--- src/src/Controller/AlbumController.php
+++ src/src/Controller/AlbumController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Album;
+use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +14,29 @@ class AlbumController extends AbstractController
     /**
      * @Route("/", name="Album")
      */
-    public function index(): Response
+    public function index(AlbumRepository $albumRepository): Response
     {
-        $sabbath = new Album("Black sabbath","Black sabbath", "https://2.bp.blogspot.com/-PnW1RODaxoY/UeFsE-z-aJI/AAAAAAAAT6g/xOic4T785Gc/s1600/Black-Sabbath-album-cover.jpg", 10, 50);
-        $cowboys = new Album("Pantera","CowBoys From Hell", "https://img.discogs.com/L1lhVb3qk5QHpJLBoYmgUWc0_VU=/fit-in/600x515/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2534882-1350747897-4609.jpeg.jpg", 12, 90);
-        $listaAlbum = [$sabbath, $cowboys];
-
+        $listAlbum = $albumRepository->findAll();
+        
         return $this->render('home/index.html.twig', [
-            "listaAlbum" => $listaAlbum
+            "listAlbum" => $listAlbum
         ]);
     }
+
+    /**
+     * @Route("/adicionar", name="adicionar")
+     */
+    public function adicionar(Request $request, AlbumRepository $albumRepository){
+        $name = $request->get('name');
+        $band = $request->get('band');
+        $imgUrl = $request->get('imgUrl');
+        $qtdMusics = $request->get('qtdMusics');
+        $playTime = $request->get('playTime');
+        $album = new Album($band, $name, $imgUrl,$qtdMusics, $playTime);
+
+        $albumRepository->save($album);
+
+        return $this->redirectToRoute("Album");
+    }
+
 }
---
+++ src/src/Repository/AlbumRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Album;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class AlbumRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Album::class);
+    }
+
+    public function save(Album $album)
+    {
+        $em = $this->getEntityManager();
+        $em->beginTransaction();
+        $em->persist($album);
+        $em->flush();  
+    }
+}
