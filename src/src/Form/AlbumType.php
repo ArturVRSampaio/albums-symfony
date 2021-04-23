7bd634c9a545613684ca71094a30e22b7795f5ee
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-23 13:59:49 -0300

    data types

--- src/src/Form/AlbumType.php
+++ src/src/Form/AlbumType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AlbumType extends AbstractType
@@ -12,11 +14,11 @@ class AlbumType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('band')
-            ->add('name')
-            ->add('imgUrl')
-            ->add('qtdMusics')
-            ->add('playTime')
+            ->add('band', TextType::class)
+            ->add('name', TextType::class)
+            ->add('imgUrl', TextType::class)
+            ->add('qtdMusics', NumberType::class)
+            ->add('playTime', NumberType::class)
         ;
     }
 
